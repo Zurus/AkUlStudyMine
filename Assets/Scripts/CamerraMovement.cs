e6710ecf9108adfcb73050cbc29330d618704622
WIN-34GA4MSGIH7\Artur <zulkund@mail.ru>
2025-07-19 21:32:22 +0500

    Переработано движение камеры за персонажем

---
+++ Assets/Scripts/CamerraMovement.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerraMovement : MonoBehaviour
+{
+
+    public Transform target;
+    public float smoothing; 
+
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+
+        }
+
+        
+    }
+}
