01e3e8859638417df276e184794951c732a465c7
WIN-34GA4MSGIH7\Artur <zulkund@mail.ru>
2025-07-27 21:18:52 +0500

    - Добавлены границы камеры - Добавлена музыка на задний фон

--- Assets/Scripts/CamerraMovement.cs
+++ Assets/Scripts/CamerraMovement.cs
@@ -6,7 +6,10 @@ public class CamerraMovement : MonoBehaviour
 {
 
     public Transform target;
-    public float smoothing; 
+    public float smoothing;
+
+    public Vector2 maxPosition;
+    public Vector2 minPosition;
 
 
 
@@ -21,6 +24,11 @@ public class CamerraMovement : MonoBehaviour
         if (transform.position != target.position)
         {
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+
+
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
 
         }
